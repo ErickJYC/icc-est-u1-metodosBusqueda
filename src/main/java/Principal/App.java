d8f9bc9a1e18254f76bf75cb695f89c3d830e2b5
Erick Yunga <eyungac3@est.ups.edu.ec>
2025-04-15 07:43:47 -0500

    FIX(001): Inicio

--- src/main/java/Principal/App.java
+++ src/main/java/Principal/App.java
@@ -1,7 +1,15 @@
 package Principal;
 
-import views.ShowConsole;
+import controles.MetodoBusqueda;
+
+import java.util.Scanner;
 
 public class App {
+    public static void main(String[] args) {
+        int [] numero = {15,11,5,20,2};
+        Scanner scanner = new Scanner(System.in);
+        MetodoBusqueda metodoBusqueda = new MetodoBusqueda();
+
+    }
 
 }
